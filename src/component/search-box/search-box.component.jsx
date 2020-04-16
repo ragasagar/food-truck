b76764415d782af282284dfe17f9b22472362768
ragasagar <sagarpoudel2052@gmail.com>
2020-04-16 17:51:23 +0530

    Location based Search

--- src/component/search-box/search-box.component.jsx
+++ src/component/search-box/search-box.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./search-box.style.scss";
 
-export const SearchBox = ({ placeholder, handleChange }) => (
+export const SearchBox = ({ id, placeholder, handleChange, value }) => (
     <input
+        id={id}
         type="search"
         placeholder={placeholder}
         className="search searchBox"
         onChange={handleChange}
+        value={value}
     />
 )
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,18 +4,20 @@ import TruckMap from '../../component/map/truck-map.component.jsx';
 import ApiCaller from '../../service/api-caller.service';
 import { SearchBox } from '../../component/search-box/search-box.component';
 import SelectBox from '../../component/select-box/select-box.component';
-
+import { GoogleApiWrapper } from 'google-maps-react';
+import Script from 'react-load-script';
 
 class HomePage extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             trucks: [],
             currentLocation: {
                 lat: 37.7749, lng: -122.4194
             },
             searchInput: "",
-            selectValue: ""
+            selectValue: "",
+            query: ""
         }
         this.apicaller = new ApiCaller();
         this.truckRef = React.createRef();
@@ -26,7 +28,6 @@ class HomePage extends Component {
         this.apicaller.fetchTrucks().then(response => this.setState({
             trucks: response
         }));
-
     }
 
     getLocation = () => {
@@ -77,6 +78,29 @@ class HomePage extends Component {
         return Value * Math.PI / 180;
     }
 
+    loadScript = () => {
+        console.log(this.props.google);
+        this.searchLocation = new this.props.google.maps.places.Autocomplete(document.getElementById('autocomplete'), { types: ['geocode'] });
+        this.searchLocation.addListener('place_changed', this.handleSearch);
+    }
+
+    handleSearch = (event) => {
+        const geolocation = this.searchLocation.getPlace();
+        if (geolocation) {
+            this.setState({
+                currentLocation: {
+                    lat: geolocation.geometry.location.lat(),
+                    lng: geolocation.geometry.location.lng(),
+                },
+                query: geolocation.formatted_address
+            })
+        }
+        else {
+            this.setState({
+                query: event.target.value
+            })
+        }
+    }
     render() {
         const { trucks, currentLocation, searchInput, selectValue } = this.state
         const filteredTrucks = trucks
@@ -84,10 +108,19 @@ class HomePage extends Component {
             .filter(truck => truck.facilitytype && truck.facilitytype.toLowerCase().includes(selectValue.toLowerCase()))
             .filter(truck => this.distance(currentLocation.lat, currentLocation.lng, truck.latitude, truck.longitude) <= 2);
         return <div className="homepage">
+            <Script url="https://maps.googleapis.com/maps/api/js?libraries=places,geometry&key=AIzaSyB7FLBGtiHmsV4pfleTY35haRoDEYcwlY4"
+                onLoad={this.loadScript} />
             <div className="search-food">
                 <SearchBox
                     placeholder="Search Food Type"
                     handleChange={this.handleChange} />
+                <SearchBox
+                    id="autocomplete"
+                    type="input"
+                    placeholder="Seach City"
+                    handleChange={this.handleSearch}
+                    value={this.state.query}
+                />
                 <SelectBox onChange={this.onSelect} />
             </div>
             <TruckMap trucks={filteredTrucks} location={currentLocation} ref={this.truckRef} />
@@ -96,4 +129,6 @@ class HomePage extends Component {
     }
 }
 
-export default HomePage;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyB7FLBGtiHmsV4pfleTY35haRoDEYcwlY4'
+})(HomePage);
